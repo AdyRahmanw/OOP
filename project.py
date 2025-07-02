ff089fd1310c5214a710523cf74b89cd0c5a77c3
rifkyaghniyanugraha <rifky.a.nugraha@gmail.com>
2025-07-02 12:06:56 +0700

    #Progress1 - Pembuatan class, atribut dan method
    Mantap, lanjutkan

--- project.py
+++ project.py
@@ -1,18 +1,22 @@
-class user:
+class user: #Setiap nama class diawali huruf Kapital.
     def __init__(self,nama_lengkap,email,no_telepon,nip,role):
         self.nama = nama_lengkap
-        self.email = email
-        self.no_telepon = no_telepon
+        #email dan notelepon apakah harus semua user punya atau tidak? Pertimbangkan lagi apakah turunan dari user(admin dan kepsek) perlu email dan notelepon?
+        self.email = email 
+        self.no_telepon = no_telepon 
         self.nip = nip
         self.role = role
     
     def login(self):
         print(f"kosongan dulu")
-class guru(user):
+
+    #Tambahkan method logout juga jika ada method login
+
+class guru(user): #Setiap nama class diawali huruf Kapital.
     def __init__(self,nama_lengkap,email,no_telepon,nip,role,mapel,total_pendaaptan,jumlah_sesi):
         super().__init__(nama_lengkap,email,no_telepon,nip,role)
         self.mapel = mapel
-        self.total_pendapatan = total_pendaaptan
+        self.total_pendapatan = total_pendaaptan #Gunakan enkapsulapsi
         self.jumlah_sesi = jumlah_sesi
     
     def hitung_pendapatan_akhir(self):
@@ -23,19 +27,19 @@ class guru(user):
     def get_info_dari_data_rekap(self):
         ...
      
-class kepsek(user):
+class kepsek(user): #Setiap nama class diawali huruf Kapital.
     def __init__(self, nama_lengkap, email, no_telepon, nip, role,jabatan):
         super().__init__(nama_lengkap, email, no_telepon, nip, role)
         self.jabatan = jabatan
     
-    def melihat_data_rekap(self):
+    def melihat_data_rekap(self): #Polymorpisme aja dari method lihat data rekap dari class guru
         ...
         
-class admin(user):
-    def __init__(self, nama_lengkap, email, no_telepon, nip, role):
-        super().__init__(nama_lengkap, email, no_telepon, nip, role)  
+class admin(user): #Setiap nama class diawali huruf Kapital.
+    def __init__(self, nama_lengkap, email, no_telepon, nip, role): #Kalau tidak ada atribut tambahan tidak perlu di-override methodnya.
+        super().__init__(nama_lengkap, email, no_telepon, nip, role) 
     
-    def melihat_data_rekap(self):
+    def melihat_data_rekap(self): #Polymorpisme aja dari method lihat data rekap dari class guru
         ...
     def menghapus_guru(self):
         ...
@@ -43,5 +47,6 @@ class admin(user):
         ...
     def mengubah_data_rekap(self):      
         ...
-
+#Pertimbangkan ada class untuk honorarium, berfungsi untuk menyimpan semua data dan behavior dari rekap sesi guru.
+#Coba upload juga class diagramnya Adi
         
